cbd8bfda361d19c04d712edf78c59a45bf4ca24d
Jazmine <lm28810@gmail.com>
2022-11-19 14:58:51 -0500

    final commit

--- Loops.js
+++ Loops.js
@@ -56,4 +56,31 @@ const runLaps = () =>{
 }
 
 runLaps()
-Complete questions   9
+
+const invention = () =>{
+    for(let i=1900; i<1950; i++){
+        if(i === 1900){
+            console.log("Zeppline was created in 1900")
+        }else if (i === 1902){
+            console.log("The Teddy Bear was created in 1902")
+        } else if (i === 1910){
+            console.log("The talking motion picture was created in 1910")
+        }else if (i === 1913){
+            console.log("THe Bra was created in 1913")
+        } else if (i === 1918){
+            console.log("The fortune cookie was created in 1918")
+        } else if (i === 1923){
+            console.log("The traffic signal was created in 1923")
+        } else if (i === 1935){
+            console.log("The Radar was created in 1935")
+        } else if (i === 1938){
+            console.log("The Ballpoint Pen was created in 1938")
+        } else if(i === 1943){
+            console.log("The slinky was created in 1943")
+        } else {
+        console.log(i)
+        }
+    }
+}
+
+invention()
